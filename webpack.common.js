89ebd67d6b218d04c7fe8c3f174e2e495f30165b
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2023-12-22 15:45:26 +0200

    inital setup

--- webpack.common.js
+++ webpack.common.js
@@ -14,12 +14,22 @@ module.exports = {
   module: {
 
     rules: [
-
       {
 
         test: /\.css$/i,
 
         use: ['style-loader', 'css-loader'],
+
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
 
       },
 
